299d7c658511117ab4588e70aa88be6f54496754
Sabrina Stangler <sabrinastangler51@gmail.com>
2023-05-18 08:43:06 -0500

    more cleanup

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,15 +1,13 @@
-import Icon from '../../assets/curiousbean-icon-color.svg'
 import './footer.css'
 
 export function Footer() {
   return (
     <div className="footer">
-      <img src={Icon} alt="curiousbean-icon" height="100vh" />
       <p>
-        <a className="email-link" href="mailto:sabstangler@gmail.com">Email me</a> if you'd like to work together!
+        Check out the <a href="https://github.com/sabrina-stangler/dotcom">github repo</a> for this website.
       </p>
       <p>
-        Check out the <a className="email-link" href="https://github.com/sabrina-stangler/curiousbeandev">github repo</a> for this website.
+        Business Website: <a href="https://curiousbean.dev">curiousbean.dev</a>
       </p>
     </div>
   )
